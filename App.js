4ed18f1d0f0d9f8578f401455711c235cf2c7056
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-10-10 14:40:53 +0200

    productitem view is done, refactor needed.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { AsyncStorage }  from 'react-native';
 export default function App() {
 
   // Create a local storage cart for the user if not exists
+  //AsyncStorage.clear()
   try{
     const cart = JSON.parse(AsyncStorage.getItem("userCart"));
   } catch (error){
--- src/screens/products/ProductItemScreen.js
+++ src/screens/products/ProductItemScreen.js
@@ -8,65 +8,65 @@ import NumericInput from 'react-native-numeric-input';
 export const ProductItemScreen = ({ route, navigation }) => {
 
     const product = route.params;
-    const [orderCount,setOrderCount] = React.useState(0);
-    let itemFound = false;
+    const [orderCount,setOrderCount] = React.useState(0); //order count from input
+    const [errorText, setErrorText] = React.useState(""); //error text in case of 0 input
+    let itemFound = false; // product search in cart
 
+    // orderCount input handling
+    function handleOrderCount(value){
+        setOrderCount(value);
+        if(value !== 0)
+            setErrorText("");
+    }
 
-
+    // addToCart button (icon) handling
     async function addToCartHandler(){
-        //add to cart logic
-        //if the product is in the cart, increment the value count by 1
+        //if the product is in the cart, increment the value count by 1, if it is not, place the product in the cart
+        if(orderCount === 0) {
+            setErrorText("Please enter a number greater than 0!"); //make sure to not put quantity 0 in the cart
+        } else {
+            let listOfProducts = JSON.parse(await AsyncStorage.getItem("userCart")); //get the user cart
 
-
-
-        let listOfProducts = JSON.parse(await AsyncStorage.getItem("userCart")); //get the user cart
-
-        console.log("after get:")
-        console.log(listOfProducts);
-
-        if(listOfProducts.length > 1 ) { //case : the user cart is not empty
-            console.log("bemegy ahova nem kéne")
-            for (let i = 0; i < listOfProducts.length;i++) { // going through the products
-                console.log(listOfProducts[i])
-                if(product.name ===  JSON.parse(listOfProducts[i]).name){ //the current product is in the cart
-                    itemFound = true;
-                    const product = JSON.parse(listOfProducts[i]);
-                    product.quantity += orderCount; //add to order count to the quantity
-                    listOfProducts[i] = JSON.stringify(product);
-                    await AsyncStorage.setItem("userCart", JSON.stringify(listOfProducts)); //store in the cart
+            //case : the user cart is not empty
+            if( (listOfProducts.length >= 1 && listOfProducts[0] !== "")  || listOfProducts[0] !== "" ) {
+                for (let i = 0; i < listOfProducts.length;i++) { // going through the products
+                    if(product.name ===  JSON.parse(listOfProducts[i]).name){ //the current product is in the cart
+                        itemFound = true; //we have found our item
+                        const product = JSON.parse(listOfProducts[i]);
+                        product.quantity += orderCount; //add to order count to the quantity
+                        listOfProducts[i] = JSON.stringify(product);
+                        await AsyncStorage.setItem("userCart", JSON.stringify(listOfProducts)); //store in the cart
+                    }
                 }
             }
+
+            //case : our item is not found in the cart or empty cart
+            if((itemFound === false && (listOfProducts.length === 1 || listOfProducts[0] === "")) || (itemFound === false && listOfProducts.length > 1)) {
+                let cart = JSON.parse(await AsyncStorage.getItem("userCart")); // get the cart
+
+                const itemToPurchase = JSON.stringify({
+                    id:product.id,
+                    name:product.name,
+                    price:product.price,
+                    image:product.image,
+                    quantity: orderCount
+                });
+
+                cart.push(itemToPurchase);
+                if(listOfProducts[0] === "") {
+                    // remove the empty item from our list (localstorage cart creation. App.js line:13
+                    cart = cart.slice(1);
+                }
+
+                await AsyncStorage.setItem("userCart", JSON.stringify(cart)); //push the item to cart
+            }
         }
-
-        if(itemFound === false || listOfProducts.length === 1) { //case : our item is not found in the list or empty
-            console.log("bemegy az ifbee")
-            let cart = JSON.parse(await AsyncStorage.getItem("userCart")); // get the cart
-
-            const itemToPurchase = JSON.stringify({
-                id:product.id,
-                name:product.name,
-                price:product.price,
-                image:product.image,
-                quantity: orderCount
-            });
-
-            cart.push(itemToPurchase);
-
-            await AsyncStorage.setItem("userCart", JSON.stringify(cart)); //push the item to cart
-
-            //this route works fine
-        }
-
-        console.log("user cart after insert:");
         console.log(JSON.parse(await AsyncStorage.getItem("userCart")));
-
-       //redirect to productsScreen
     }
 
     return (
         <SafeAreaView>
             <StatusBar style="dark" />
-
             <ProductItem
             id={product.id}
             name={product.name}
@@ -74,7 +74,8 @@ export const ProductItemScreen = ({ route, navigation }) => {
             image={product.image}
             />
 
-            <NumericInput onChange={ value => setOrderCount(value) } />
+            <NumericInput onChange={ value => handleOrderCount(value) } />
+            {errorText !== "" && <Text style={styles.errorText}>{errorText}</Text>}
 
             <Text>Shop now!</Text>
             <TouchableOpacity
@@ -86,7 +87,6 @@ export const ProductItemScreen = ({ route, navigation }) => {
                     source={{ uri: 'https://cdn0.iconfinder.com/data/icons/webshop-essentials/100/shopping-cart-512.png' }}
                 />
             </TouchableOpacity>
-            <Text>asd</Text>
         </SafeAreaView>
     );
 }
@@ -100,5 +100,8 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40,
     },
+    errorText: {
+        color: "red"
+    }
 });
 
